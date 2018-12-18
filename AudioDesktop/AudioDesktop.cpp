871f9acc7f2936ec2e81ac38711d7b22af08ef72
kxykzy <895542534@qq.com>
2018-12-18 21:52:33 +0800

    Test

--- AudioDesktop/AudioDesktop.cpp
+++ AudioDesktop/AudioDesktop.cpp
@@ -1,7 +1,33 @@
 #include "AudioDesktop.h"
 
 AudioDesktop::AudioDesktop(QWidget *parent)
-	: QWidget(parent)
+	: QOpenGLWidget(parent)
 {
 	ui.setupUi(this);
+	//setGeometry(0, 0, qApp->desktop()->width(), qApp->desktop()->height());
+	setWindowFlags(windowFlags()
+		| Qt::FramelessWindowHint //去边框
+		| Qt::X11BypassWindowManagerHint //linux下脱离任务管理器
+		| Qt::WindowStaysOnBottomHint //最低层显示
+		//| Qt::Tool //不在任务栏显示
+	);
+	setAttribute(Qt::WA_TranslucentBackground);
+	setWindowState(Qt::WindowNoState //不激活
+		| Qt::WindowFullScreen //全屏
+	);
+	setFocusPolicy(Qt::NoFocus);
+	//setWindowOpacity(WINDOW_OPACITY);
+}
+
+void AudioDesktop::initializeGL()
+{
+}
+
+void AudioDesktop::resizeGL(int w, int h)
+{
+}
+
+
+void AudioDesktop::paintGL()
+{
 }
--- AudioDesktop/AudioDesktop.h
+++ AudioDesktop/AudioDesktop.h
@@ -3,13 +3,17 @@
 #include <QtWidgets/QWidget>
 #include "ui_AudioDesktop.h"
 
-class AudioDesktop : public QWidget
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+class AudioDesktop : public QOpenGLWidget, protected QOpenGLFunctions
 {
 	Q_OBJECT
 
 public:
 	AudioDesktop(QWidget *parent = Q_NULLPTR);
-
+	void initializeGL();
+	void resizeGL(int w, int h);
+	void paintGL();
 private:
 	Ui::AudioDesktopClass ui;
 };
