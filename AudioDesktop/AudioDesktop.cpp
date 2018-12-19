e4540a0a0a754af31f8ab6113fce5e1ab1088c88
kxykzy <895542534@qq.com>
2018-12-19 21:42:35 +0800

    ShaderInDesktop

--- AudioDesktop/AudioDesktop.cpp
+++ AudioDesktop/AudioDesktop.cpp
@@ -1,5 +1,23 @@
 #include "AudioDesktop.h"
+#include <QtCore/QCoreApplication>
+#include <QtGui/QOpenGLShaderProgram>
+#include <QtGui/QOpenGLContext>
+#include <QtGui/QScreen>
+static const char *vertexShaderSource =     // 着色器语言
+"attribute highp vec4 posAttr;\n"
+"attribute lowp vec4 colAttr;\n"
+"varying lowp vec4 col;\n"
+"uniform highp mat4 matrix;\n"
+"void main() {\n"
+"   col = colAttr;\n"
+"   gl_Position = matrix * posAttr;\n"
+"}\n";
 
+static const char *fragmentShaderSource =   // 着色器语言
+"varying lowp vec4 col;\n"
+"void main() {\n"
+"   gl_FragColor = col;\n"
+"}\n";
 AudioDesktop::AudioDesktop(QWidget *parent)
 	: QOpenGLWidget(parent)
 {
@@ -8,26 +26,83 @@ AudioDesktop::AudioDesktop(QWidget *parent)
 	setWindowFlags(windowFlags()
 		| Qt::FramelessWindowHint //去边框
 		| Qt::X11BypassWindowManagerHint //linux下脱离任务管理器
-		| Qt::WindowStaysOnBottomHint //最低层显示
+		//| Qt::WindowStaysOnBottomHint //最低层显示
 		//| Qt::Tool //不在任务栏显示
 	);
 	setAttribute(Qt::WA_TranslucentBackground);
 	setWindowState(Qt::WindowNoState //不激活
-		| Qt::WindowFullScreen //全屏
+		//| Qt::WindowFullScreen //全屏
 	);
 	setFocusPolicy(Qt::NoFocus);
-	//setWindowOpacity(WINDOW_OPACITY);
+	//setWindowOpacity(0.5);
+}
+
+AudioDesktop::~AudioDesktop()
+{
+	if (m_context)
+		delete m_context;
 }
 
 void AudioDesktop::initializeGL()
 {
+	initializeOpenGLFunctions();
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_2D);
+
 }
 
 void AudioDesktop::resizeGL(int w, int h)
 {
+	glViewport(0, 0, w, h);
+	m_program = new QOpenGLShaderProgram(this);
+	m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+	m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+	m_program->link();
+	m_posAttr = m_program->attributeLocation("posAttr");
+	m_colAttr = m_program->attributeLocation("colAttr");
+	m_matrixUniform = m_program->uniformLocation("matrix");
 }
 
 
 void AudioDesktop::paintGL()
 {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	const qreal retinaScale = devicePixelRatio();
+	glViewport(0, 0, width() * retinaScale, height() * retinaScale);
+	glClear(GL_COLOR_BUFFER_BIT);
+	m_program->bind();
+
+	QMatrix4x4 matrix;
+	matrix.perspective(60.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	matrix.translate(0, 0, -2);
+	matrix.rotate(100.0f * m_frame++ , 0, 1, 0);
+
+	m_program->setUniformValue(m_matrixUniform, matrix);
+
+	GLfloat vertices[] = {
+		0.0f, 0.707f,
+		-0.5f, -0.5f,
+		0.5f, -0.5f
+	};
+
+	GLfloat colors[] = {
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f
+	};
+
+	glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, vertices);
+	glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, colors);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(0);
+
+	m_program->release();
+
 }
--- AudioDesktop/AudioDesktop.h
+++ AudioDesktop/AudioDesktop.h
@@ -1,19 +1,32 @@
-#pragma once
-
 #include <QtWidgets/QWidget>
 #include "ui_AudioDesktop.h"
-
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
+QT_BEGIN_NAMESPACE
+class QPainter;
+class QOpenGLContext;
+class QOpenGLPaintDevice;
+class QOpenGLShaderProgram;
+QT_END_NAMESPACE
 class AudioDesktop : public QOpenGLWidget, protected QOpenGLFunctions
 {
 	Q_OBJECT
 
 public:
 	AudioDesktop(QWidget *parent = Q_NULLPTR);
+	~AudioDesktop();
 	void initializeGL();
 	void resizeGL(int w, int h);
 	void paintGL();
 private:
 	Ui::AudioDesktopClass ui;
+	QOpenGLContext *m_context;              // 画板对象
+	QOpenGLShaderProgram *m_program;        // 着色器(画刷)对象
+
+	GLuint m_posAttr;                       // 着色器参数：属性
+	GLuint m_colAttr;                       // 着色器参数：纹理
+	GLuint m_matrixUniform;                 // 着色器参数：uniform
+
+	int  m_frame;                           // 循环变量，只要到一次
+	bool m_update_pending;                  // 绘图完成标志(窗口更新标志)
 };
